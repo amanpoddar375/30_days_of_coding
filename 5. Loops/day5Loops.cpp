5d960a505a2f42c612d277cdca72140ec3f5d474
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-06-07 21:10:29 +0545

    Create day5Loops.cpp

---
+++ 5. Loops/day5Loops.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    int i;
+    for(i=1;i<=10;i++){
+        int n1 = 0;
+        n1 = n*i;
+        cout<<n<<" x " <<i<<" = "<<n1<<endl;
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}

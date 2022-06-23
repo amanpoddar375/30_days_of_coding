0da1c81fc43dca7222f85b5dca06dee536a691fd
Aman Poddar <46098462+amanpoddar375@users.noreply.github.com>
2022-06-23 19:52:47 +0545

    Added Binary Numbers Solution

---
+++ 10. Binary Numbers/day10BinaryNumbers.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    int rem=0,a=0,b=0;
+
+    while(n>0) {
+        rem=n%2;
+        n=n/2;
+        if(rem==1) {
+           a++;
+           if(a>=b)
+            b=a;
+        }
+        else {
+            a=0;
+        }   
+    }
+    
+    cout<<b;
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
